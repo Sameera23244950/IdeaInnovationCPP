b9479553a08af43400d62a079a4ee81c58f44743
EC2 Default User <ec2-user@ip-172-31-46-113.eu-west-1.compute.internal>
2024-04-20 02:20:50 +0000

    Library implemented

--- ideasapp/models.py
+++ ideasapp/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Feedback(models.Model):
     name=models.CharField(max_length=100)
     email=models.EmailField()
-    title=models.TextField()
     feedback=models.TextField()
     
    
--- ideasapp/views.py
+++ ideasapp/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render, HttpResponse
 from .models import Feedback
+from numbers_properties_pkg import numbers_properties
+
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
-    
+    app_name = "ideasapp"  # Your Django app name
+    model_name = "Feedback"  # The model name within your app
+ 
+    count = numbers_properties.FeedbackCounter.count_titles(app_name,model_name)
+    return render(request, 'index.html', {'feedback_count': count})
+
+
 def feedback(request):
     if request.method=="POST":
         name=request.POST["name"]
         email=request.POST["email"]
         # title=request.POST["title"]
         feedback=request.POST["feedback"]
+        
         obj=Feedback(name=name, email=email, feedback=feedback)
         obj.save()
         return HttpResponse("<h1> Idea has been submitted </h1>")
-        
-        # print(f"Name: {name}, Email: {email}, Feedback: {feedback}")
-        
+
     return render(request,'feedback.html')
